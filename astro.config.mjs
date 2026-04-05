2d679742533f3b420c7969b754b4511050c991ec
Cláudio Raposo <claudioraposo@users.noreply.github.com>
2026-04-04 23:03:01 -0300

    chore: set base to root for custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: process.env.NODE_ENV === 'production'
-    ? 'https://sertaoseracloud.github.io'
-    : 'http://localhost:4321',
-  base: process.env.NODE_ENV === 'production' ? '/blog_pessoal' : '',
+  site: 'https://mentoria.sertaoseracloud.com',
+  base: '',
 });
